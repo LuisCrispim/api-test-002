cc7f55628c69ea1b8ad6980f5eac82a7df2f9f1b
unknown <fcrispim2012@gmail.com>
2024-03-26 14:25:22 -0300

    início da implementação de rotas e migrations

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 const { Router } = require("express");
+const UsersModel = require("./apps/models/Users");
 
 const routes = new Router();
+
+routes.get("/users", async (req, res) => {
+  const allUsers = await UsersModel.findAll();
+  res.send({ users: allUsers });
+});
 
 routes.get("/health", (req, res) => {
   return res.send({ message: "Connected with success!" });
